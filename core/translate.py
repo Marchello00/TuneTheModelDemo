00a66531a5611ce3c89c18e776624f835513bacd
Mark Nagovitsin <nagovitsin.ms@phystech.edu>
2022-10-11 12:16:52 +0300

    fix cloud translate

--- core/translate.py
+++ core/translate.py
@@ -4,30 +4,34 @@ from dotenv import load_dotenv
 
 load_dotenv()
 IAM_TOKEN = os.environ.get('IAM_TOKEN')
+# TRANSLATE_API_KEY = os.environ.get('TRANSLATE_API_KEY')
 RESOURCE_MANAGER_API = \
         "https://resource-manager.api.cloud.yandex.net/resource-manager/v1"
 TRANSLATE_API = "https://translate.api.cloud.yandex.net/translate/v2/translate"
 
 
-def get_cloud():
-    headers = {
+def get_headers(iam=False):
+    return {
         "Content-Type": "application/json",
         "Authorization": "Bearer {0}".format(IAM_TOKEN)
+        # "Authorization": f"Bearer {IAM_TOKEN}" if iam else f"Api-Key {TRANSLATE_API_KEY}"
+        # "Authorization": f"Api-Key {TRANSLATE_API_KEY}"
     }
+
+
+def get_cloud():
+    headers = get_headers(iam=True)
 
     responce = requests.get(f"{RESOURCE_MANAGER_API}/clouds", headers=headers)
 
-    return responce.json()["clouds"][0]["id"]
+    return responce.json()["clouds"][-1]["id"]
 
 
 def get_folder(cloud_id=None):
     if cloud_id is None:
         cloud_id = get_cloud()
 
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer {0}".format(IAM_TOKEN)
-    }
+    headers = get_headers(iam=True)
     params = {
         'cloudId': cloud_id
     }
@@ -37,7 +41,6 @@ def get_folder(cloud_id=None):
         headers=headers,
         params=params
     )
-
     return responce.json()["folders"][0]["id"]
 
 
@@ -51,10 +54,7 @@ def translate(texts, target_language='ru', folder_id=None):
         "folderId": folder_id,
     }
 
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer {0}".format(IAM_TOKEN)
-    }
+    headers = get_headers(iam=True)
 
     response = requests.post(
         TRANSLATE_API,
